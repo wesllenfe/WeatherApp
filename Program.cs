201586aa44c7477deb8311378e0e977a1f8a9419
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-01-20 11:28:05 -0300

    aplicativo de previsão do tempo

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Write("Informe o nome da cidade: ");
+        string cityName = Console.ReadLine();
+
+        Console.WriteLine($"Obtendo informações climáticas da cidade {cityName}");
+
+        string apiKey = Environment.GetEnvironmentVariable("OPEN_WEATHER_API_KEY");
+
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            Console.WriteLine("Chave de API não encontrada. Certifique-se de definir a variável de ambiente OPEN_WEATHER_API_KEY.");
+            return;
+        }
+
+        string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var weatherData = await response.Content.ReadFromJsonAsync<WeatherData>();
+
+                    Console.WriteLine($"Condição: {weatherData.Weather[0].Description}");
+                    Console.WriteLine($"Temperatura: {weatherData.Main.Temp}ºC");
+                    Console.WriteLine($"Umidade:  {weatherData.Main.Humidity}%");
+                    double windSpeedKM = weatherData.Wind.Speed * 3.6;
+                    Console.WriteLine($"Velocidade do Vento: {weatherData.Wind.Speed} m/s ou {windSpeedKM:F2} KM/H");
+                }
+                else
+                {
+                    Console.WriteLine($"Erro na requisição: {response.ReasonPhrase}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Erro na requisição: {ex.Message}");
+            }
+        }
+    }
+}
+
+public class WeatherData
+{
+    public Weather[] Weather { get; set; }
+    public Main Main { get; set; }
+    public Wind Wind { get; set; }
+}
+
+public class Weather
+{
+    public string Description { get; set; }
+}
+
+public class Main
+{
+    public float Temp { get; set; }
+    public int Humidity { get; set; }
+}
+
+public class Wind
+{
+    public float Speed { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# WeatherApp
+
+Um aplicativo de console simples para obter informações climáticas usando a OpenWeatherMap API.
+
+## Configuração
+
+Antes de executar o aplicativo, você precisará de uma chave de API da OpenWeatherMap.
+
+1. Vá para [OpenWeatherMap](https://openweathermap.org/api) e crie uma conta gratuita.
+2. Obtenha sua chave de API.
+
+## Configuração da Variável de Ambiente
+
+Defina a variável de ambiente `OPEN_WEATHER_API_KEY` com sua chave de API.
+
+**Exemplo no Windows:**
+
+```powershell
+setx OPEN_WEATHER_API_KEY "sua_chave_de_api"
+```
+
+**Executando o Aplicativo:**
+
+```No Terminal
+dotnet run
+```
